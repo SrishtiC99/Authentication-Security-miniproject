cd4531c6abe67d12c72da47cf583c753c522cec0
SrishtiC99 <iamsri9631@gmail.com>
2023-01-04 13:01:45 +0530

    Level-2 auth using database encryption

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', true);
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 app.use(bodyParser.urlencoded({
@@ -20,6 +22,8 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
